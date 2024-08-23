2a9949073c906be86688a4cc181aa70e192e7754
Kyle Bennett <kylebennett00001@gmail.com>
2024-08-23 14:04:09 -0400

    doc: add a template for jest test

---
+++ src/lib/template.jest.ts
@@ -0,0 +1,18 @@
+// This file contains a template for a jest test file. It is used to create test files for missing tests.
+//
+
+import fs from 'fs';
+import { logger } from '../index'
+
+export const jestTemplate = (relPath: string) => {
+    return `
+        import { describe, expect, test } from '@jest/globals';
+        import * as SUT from '${relPath}';
+
+        describe('SYSTEM_UNDER_TEST', () => {
+            test('should do something', () => {
+                expect(SUT).toBeDefined();
+            });
+        });
+    `
+};
