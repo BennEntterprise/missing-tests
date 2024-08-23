acb56bcfffd1310612c2a97306b26585486e13db
Kyle Bennett <kylebennett00001@gmail.com>
2024-08-23 12:20:44 -0400

    fixes: assorted, see description
    (createTestFileForMissingResults.ts) - added a fail safe where the function will
    create nested directories if they don't already exist.
    
    create empty test files

--- src/utils/createTestFilesForMissingResults.ts
+++ src/utils/createTestFilesForMissingResults.ts
@@ -5,6 +5,14 @@ function createTestFilesForMissingResults(results: Record<string, null | string>
     for (const [srcFile, testFile] of Object.entries(results)) {
         if (testFile === null) {
             const newTestFile = srcFile.replace(srcDir, testsDir).replace('.ts', '.test.ts');
+            // Ensure that the required directories exist, if they don't already, then create the required nesting.
+            const newTestDir = newTestFile.split('/').slice(0, -1).join('/');
+            if (!fs.existsSync(newTestDir)) {
+                fs.mkdirSync(newTestDir, { recursive: true });
+                logger.log(`Did not find ${newTestDir}, creating it now`);
+            } else {
+                logger.log(`Found: ${newTestDir}, no need to create it`);
+            }
             fs.writeFileSync(newTestFile, '');
             logger.log(`Created new test file: ${newTestFile}`);
         }
